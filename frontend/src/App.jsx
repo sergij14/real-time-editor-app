f5594f6507495e50398213403fbcd751d261657d
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2025-05-30 01:12:06 +0400

    feat: created frontend routes for docs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import TextEditor from "./components/TextEditor";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import { v4 as uuidV4 } from "uuid";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to={`/docs/${uuidV4()}`} />,
+  },
+  {
+    path: "/docs/:id",
+    element: <TextEditor />,
+  },
+]);
 
 const App = () => {
-  return <TextEditor />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
