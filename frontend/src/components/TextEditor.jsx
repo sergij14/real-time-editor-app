fbe16c5fa3a388179e5adc503ad67d8b73a27544
Sergi Jajanidze <sergi.jajanidze@gmail.com>
2025-05-29 23:42:55 +0400

    feat: initiated socket.io connection from client

--- frontend/src/components/TextEditor.jsx
+++ frontend/src/components/TextEditor.jsx
@@ -1,20 +1,23 @@
 import React, { useEffect, useRef } from "react";
 import Quill from "quill";
+import { io } from "socket.io-client";
 import "quill/dist/quill.snow.css";
 
 const TextEditor = () => {
   const containerRef = useRef(null);
 
   useEffect(() => {
+    const socket = io("http://localhost:3001");
+
     const $editor = document.createElement("div");
     containerRef.current.append($editor);
-
     new Quill($editor, {
       theme: "snow",
     });
 
     return () => {
       containerRef.current.innerHTML = "";
+      socket.disconnect();
     };
   }, []);
 
